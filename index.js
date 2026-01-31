f328d27aae7e7a3dd9250f7aa9a2ab43a9a2885d
John Iceberg Velez <icevelezdev@gmail.com>
2026-01-31 11:09:31 +0800

    added handlebar ssr middleware

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { logger } from "./lib/bun/middleware/logger.js";
 import RemoteAuth from "./remote/auth.js";
 import RemoteExample from "./remote/example.js";
 import ServerSentEventHandler from "./remote/sse.js";
+import { handlebar } from "./lib/bun/middleware/handlebar.js";
 
 const database = "FAKE";
 
@@ -41,7 +42,8 @@ rpcMux.handleFunc("GET /api/sse", sse_func);
 
 const mux = new HttpMux();
 mux.handle("/", logger());
-mux.handle("/", serve('public', { useGzip: true }));
+mux.handle("/", handlebar("public", { useGzip: true }));
+mux.handle("/", serve("public", { useGzip: true }));
 mux.handle("/remote/", rpcMux.strip_prefix("/remote"));
 
 mux.serve({
